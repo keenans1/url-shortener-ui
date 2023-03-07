fdf890f7bf22ab0b841f4c83abfdb6a4cc22a11b
Keenan Southall <keenan.southall@yahoo.com>
2023-03-07 09:54:41 -0700

    Finish iteration 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -17,12 +17,27 @@ export class App extends Component {
       .then(data => this.setState({ urls: data.urls }))
   }
 
+  addUrl = (newUrl) => {
+
+    fetch('http://localhost:3001/api/v1/urls', {
+      method: 'POST',
+      body: JSON.stringify(newUrl),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(json => this.setState({ urls: [...this.state.urls, json] }))
+      .catch(err => console.log('error', err))
+  }
+
+
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm />
+          <UrlForm addUrl={this.addUrl} />
         </header>
 
         <UrlContainer urls={this.state.urls} />
--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -11,16 +11,25 @@ class UrlForm extends Component {
   }
 
   handleNameChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
+    const { name, value } = e.target
+    this.setState({ [name]: value });
   }
 
   handleSubmit = e => {
     e.preventDefault();
-    this.clearInputs();
+    if (this.state.title && this.state.urlToShorten) {
+      console.log('state', this.state)
+      const newUrl = {
+        long_url: this.state.urlToShorten,
+        title: this.state.title
+      }
+      this.props.addUrl(newUrl)
+      this.clearInputs()
+    }
   }
 
   clearInputs = () => {
-    this.setState({title: '', urlToShorten: ''});
+    this.setState({ title: '', urlToShorten: '' });
   }
 
   render() {
@@ -37,8 +46,8 @@ class UrlForm extends Component {
         <input
           type='text'
           placeholder='URL to Shorten...'
-          name='title'
-          value={this.state.title}
+          name='urlToShorten'
+          value={this.state.urlToShorten}
           onChange={e => this.handleNameChange(e)}
         />
 
@@ -50,4 +59,4 @@ class UrlForm extends Component {
   }
 }
 
-export default UrlForm;
+export default UrlForm
