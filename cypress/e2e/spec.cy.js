6e013825985b9aefdfb47ed730db1f5db6efb178
Keenan Southall <keenan.southall@yahoo.com>
2023-03-07 10:27:07 -0700

    Complete second test for form

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,21 +1,27 @@
 describe('url shortener', () => {
 
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { fixture: 'example.json' })
+    cy.intercept('GET', 'http://localhost:3001/api/v1/urls', { fixture: 'receivedinfo.json' })
 
     cy.visit('http://localhost:3000/')
   })
 
   it('Should show the website title and existing urls', () => {
     cy.get('h1').should('contain', 'URL Shortener')
-    cy.get('div[id="1"]').should('contain', 'abcd')
-    cy.get('div[id="2"]').should('contain', 'keenan')
+    cy.get('div[id="1"]').should('contain', 'http://localhost:3001/useshorturl/1')
+    cy.get('div[id="2"]').should('contain', 'http://localhost:3001/useshorturl/2')
+  })
+
+  it('Should fill out the form and the corresponding inputs should be visible in the dom', () => {
+    cy.intercept('POST', 'http://localhost:3001/api/v1/urls', { fixture: 'createdinfo.json' })
+
+    cy.get('form').within(() => {
+      cy.get('input[name="title"]').type('abraham')
+      cy.get('input[name="urlToShorten"]').type('lincoln')
+      cy.get('button').click()
+    })
+
+    cy.get('div[id="3"]').should('contain', 'http://localhost:3001/useshorturl/3')
   })
 
 })
-
-
-// When a user visits the page, they can view the page title and the existing shortened URLs
-
-// When a user visits the page, they can view the Form with the proper inputs
-// When a user fills out the form, the information is reflected in the input fields
